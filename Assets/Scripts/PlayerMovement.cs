e219df74ef9de53a3760c4b0dba4cd44902008ed
DmitriiABeliaev <dmitry.a.beliaev@gmail.com>
2023-07-09 00:59:35 -0700

    Addition to movement system
    addition of controller element and script for movement system
    
    player can move on horizontal axis and jump

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    
+    float horizontalMove = 0f;
+
+    [SerializeField] private float runSpeed = 100f;
+    private bool jump = false;
+    private bool crouch = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+    }
+}
