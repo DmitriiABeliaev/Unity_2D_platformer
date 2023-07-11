a39c86619dea35858931692d1296ec7ae4ceb6d9
DmitriiABeliaev <dmitry.a.beliaev@gmail.com>
2023-07-11 14:25:48 -0700

    made a set up for saving
    Added scripts for saving and loading player position. No options to save/load yet.

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public PlayerData(Player player)
+    {
+        position = new float[3];
+        //index 0 = x coordinate, index 1 = y coordinate, index 2 = z coordinate
+        position[0] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
+    }
+
+    public int score;
+    public float[] position;
+}
